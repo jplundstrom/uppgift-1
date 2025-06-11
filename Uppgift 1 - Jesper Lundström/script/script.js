1695a2163ff0fcd22a01d5f4e76f83c03a9cef0d
jplundstrom <jesper.lundstrom@gritacademy.se>
2025-06-11 16:57:30 +0200

    Uppgift 1

---
+++ Uppgift 1 - Jesper Lundström/script/script.js
@@ -0,0 +1,12 @@
+let foo = 0;
+let bar = 0;
+
+let result;
+
+if (bar === 0) {
+    result = "Wrong: Dividing by zero is not allowed";
+} else {
+    result = foo / bar;
+}
+
+console.log("Result", result);
